0cdb7846c0efc4a0e48419a5c6d562d963b3c5c5
preztel <60546504+preztel@users.noreply.github.com>
2021-07-02 20:21:00 +0200

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -19,10 +19,11 @@ local ESP = {
 	Circle = false,
 	Radius = 100,
 	Snaplines = false,
-	Boxes = false,
+	Boxes = true,
+	BoxesOutlines = true,
 	Chams = false,
-	Names = false,
-	Health = false,
+	Names = true,
+	Health = true,
 	Distance = false,
 	Items = false
 }
@@ -94,6 +95,60 @@ GetHealth = GetHealth or function(Player)
 	return 0
 end
 
+local GetOffSet ;
+
+GetOffset = function(Part, Position)
+	local FarPosition = CurrentCamera:WorldToViewportPoint(Vector3.new(Part.Position.X, Part.Position.Y + (Part.Size.Y / 2), Part.Position.Z))
+	return FarPosition.Y - Position.Y
+end
+
+local GetCorners ;
+
+GetCorners = function(Player)
+	local TopY = math.huge
+	local BottomY = -math.huge
+	local RightX = -math.huge
+	local LeftX = math.huge
+	local Visible = false
+	local Offsets
+	local Positions = {}
+	local Character = GetCharacter(Player)
+	for i, v in next, Character:GetChildren() do
+		if v:IsA("Part") or v:IsA("BasePart") or v:IsA("MeshPart") then
+			local Position, OnScreen = CurrentCamera:WorldToViewportPoint(v.Position)
+			Positions[v] = Position
+			Offsets = GetOffset(v, Position)
+			if OnScreen and not string.find(v.Name, "Root") then
+				if Position.Y < TopY then
+					TopY = Position.Y
+				end
+				if Position.Y > BottomY then
+					BottomY = Position.Y
+				end
+				if Position.X < LeftX then
+					LeftX = Position.X
+				end
+				if Position.X > RightX then
+					RightX = Position.X
+				end
+			else
+				Visible = OnScreen
+			end
+		end
+	end
+
+	return {
+		TopLeft = Vector2.new((LeftX + Offsets), (TopY + Offsets)),
+		TopRight = Vector2.new((RightX - Offsets), (TopY + Offsets)),  
+		BottomLeft = Vector2.new((LeftX + Offsets), (BottomY - Offsets)), 
+		BottomRight = Vector2.new((RightX - Offsets), (BottomY - Offsets)), 
+		Positions = Positions, 
+		Visible = Visible
+	}
+end
+
+local Roundify ;
+
 Roundify = function(number)
 	if (number - (number % 0.1)) - (number - (number % 1)) < 0.5 then
 		number = number - (number % 1)
@@ -105,43 +160,125 @@ end
 
 --< ESP Functions
 
+function Boxes(Player)
+	local function Boxes()
+		return false
+	end
+
+	local BoxOutline = Drawing.new("Square")
+	local Box = Drawing.new("Square")
+
+	Boxes = function()
+		if ESP.Boxes and IsPlayerAlive(Player) then
+			if Player then 
+				local Character = GetCharacter(Player)	
+				local Corners = GetCorners(Player)
+
+				if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChild("Head") then
+
+					if Player.Team ~= LocalPlayer.Team then
+						Box.Color = Color3.fromRGB(127, 94, 235)
+					else
+						Box.Color = Color3.fromRGB(235, 235, 235)
+					end
+
+					local p1, Vis = CurrentCamera:WorldToViewportPoint((Character:FindFirstChild("HumanoidRootPart").CFrame).p)
+
+					local X, Y = Corners.BottomRight.X - Corners.TopLeft.X, Corners.BottomRight.Y - Corners.TopLeft.Y
+
+					Box.Visible = Vis
+					BoxOutline.Visible = Vis
+
+					Box.Visible = true
+					Box.Size = Vector2.new(X, Y)
+					Box.Position = Corners.TopLeft
+					Box.Thickness = 1
+					Box.Filled = false
+
+					if ESP.BoxesOutlines then
+						BoxOutline.Visible = true
+						BoxOutline.Size = Vector2.new(Corners.BottomRight.X - Corners.TopLeft.X, Corners.BottomRight.Y - Corners.TopLeft.Y)
+						BoxOutline.Position = Corners.TopLeft
+						BoxOutline.Thickness = (Box.Thickness * 2.5)
+						BoxOutline.Filled = false
+					else
+						BoxOutline.Visible = false
+					end
+				else
+					Box.Visible = false
+					BoxOutline.Visible = false
+				end
+			else
+				Box.Visible = false
+				BoxOutline.Visible = false
+			end
+		else
+			Box.Visible = false
+			BoxOutline.Visible = false
+		end
+	end
+
+	math.randomseed(tick())
+	RunService:BindToRenderStep("Boxes", 1, Boxes)
+end
+
 function Names(Player)
 	local function Name()
 		return false
 	end
 
 	local NameText = Drawing.new("Text")
+	local HealthText = Drawing.new("Text")
 	NameText.Center = true
-	NameText.Text = Player.Name
-	NameText.Size = 13
+	NameText.Text = "Player"
+	--[[if string.len(Player.Name) < 10 then
+		NameText.Text = (Player.Name)
+	else
+		NameText.Text = (string.sub(Player.Name, 1, 8) .. "..")
+	end]]
+	NameText.Size = 12.7
 	NameText.Font = Drawing.Fonts.Plex
 	NameText.Outline = true
 	NameText.OutlineColor = Color3.new(0,0,0)
+	
+	HealthText.Center = true
+	HealthText.Size = 12
+	HealthText.Font = Drawing.Fonts.Plex
+	HealthText.Outline = true
+	HealthText.OutlineColor = Color3.new(0,0,0)
+	HealthText.Color = Color3.fromRGB(91, 235, 115)
 
 	Name = function()
-		if ESP.Names and Hud:isplayeralive(Player) then
+		if IsPlayerAlive(Player) then
 			if Player then 
 				local Character = GetCharacter(Player)	
 				if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChild("Head") then
 					if Player.Team ~= LocalPlayer.Team then
-						NameText.Color = Color3.fromRGB(186, 0, 0)
+						NameText.Color = Color3.fromRGB(127, 94, 235)
 					else
 						NameText.Color = Color3.fromRGB(235, 235, 235)
 					end
 
-					local Pos, Vis = CurrentCamera:WorldToViewportPoint((Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0, Character:FindFirstChild("HumanoidRootPart").Size.Y + 0.5, 0)).p)
+					local Pos, Vis = CurrentCamera:WorldToViewportPoint((Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0, Character:FindFirstChild("HumanoidRootPart").Size.Y, 0)).p)
 
-					NameText.Visible = Vis
+					NameText.Visible = Vis and ESP.Names
+					HealthText.Visible = Vis and ESP.Health
+					NameText.Text = NameText.Text
+					HealthText.Text = "[" .. GetHealth(Player) .. "]"
 
-					NameText.Position = Vector2.new(Pos.X, Pos.Y - 20)
+					NameText.Position = Vector2.new(Pos.X, Pos.Y - 15)
+					HealthText.Position = Vector2.new(NameText.Position.X, NameText.Position.Y - 18)
 				else
 					NameText.Visible = false
+					HealthText.Visible = false
 				end
 			else
 				NameText.Visible = false
+				HealthText.Visible = false
 			end
 		else
 			NameText.Visible = false
+			HealthText.Visible = false
 		end
 	end
 
@@ -192,8 +329,7 @@ if (game.PlaceId == 3233893879) then
 		end
 	end
 elseif (game.PlaceId == 292439477) then
-	getgenv().GetBodyParts = nil ;
-	getgenv().Hud = nil ;
+	local GetBodyParts, Hud ;
 
 	for i, v in next, getgc(true) do
 		if(typeof(v) == "table" and rawget(v, "getbodyparts")) then
@@ -226,10 +362,12 @@ end
 for i, v in next, Players:GetPlayers() do
 	if v ~= LocalPlayer then
 		Names(v)
+		Boxes(v)
 	end
 end
 Players.PlayerAdded:Connect(function(v)
 	Names(v)
+	Boxes(v)
 end)
 
 return Aimbot, ESP, FOV	;
